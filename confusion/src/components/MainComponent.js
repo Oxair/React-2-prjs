fe8d165bf2da636e90132cb3509d5c0e68586701
Oxair <pk.ozair@gmail.com>
2019-10-28 05:08:37 +0500

    2 Routing

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import { DISHES } from '../shared/dishes'
 import Menu from './MenuComponent';
 import DishDetailComponent from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      dishes: DISHES,
-      selectedDish: null
+      dishes: DISHES
     };
   }
 
-  onDishSelect(dishId) {
-      this.setState({ selectedDish: dishId });
-  }
-
   render() {
+    const HomePage = () => {
+      return (
+        <Home />
+      )
+    }
+
     return (
       <div>
         <Header />
-        <Menu 
-            dishes={ this.state.dishes } 
-            onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetailComponent 
-            dish={ this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0] } />
+        <Switch>
+          <Route path='/home' component={ HomePage } />
+          <Route exact path='/menu' component={() => <Menu dishes={ this.state.dishes} />} />
+          <Route to='/home' />
+        </Switch>
         <Footer />
       </div>
     );
