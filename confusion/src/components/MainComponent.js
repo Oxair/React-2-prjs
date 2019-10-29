24028be8a53417582952878a8bd657c1b293bb40
Oxair <pk.ozair@gmail.com>
2019-10-29 17:12:00 +0500

    2 SPA

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
-import { DISHES } from '../shared/dishes'
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import Menu from './MenuComponent';
-import DishDetailComponent from './DishDetailComponent';
+import DishDetail from './DishDetailComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
@@ -12,15 +16,30 @@ class Main extends Component {
     super(props);
 
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
   render() {
     const HomePage = () => {
       return (
-        <Home />
+        <Home 
+        dish={ this.state.dishes.filter((dish) => dish.featured)[0] }
+        promotion={ this.state.promotions.filter((promo) => promo.featured)[0] }
+        leader={ this.state.leaders.filter((leader) => leader.featured)[0] }
+        />
       )
+    }
+
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail dish={ this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0] } 
+          comments={ this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10)) }  
+        />
+      );
     }
 
     return (
@@ -29,6 +48,8 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={ HomePage } />
           <Route exact path='/menu' component={() => <Menu dishes={ this.state.dishes} />} />
+          <Route path='/menu/:dishId' component={ DishWithId } />
+          <Route exact path='/contactus' component={ Contact } />
           <Route to='/home' />
         </Switch>
         <Footer />
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,44 +1,53 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import DishDetail from './DishDetailComponent';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-class Menu extends Component {
-	constructor(props) {
-		super(props);
+function RenderMenuItem({ dish, onClick }) {
+	return(
+		<Card>
+			<Link to={ `/menu/${ dish.id }` }>
+				<CardImg width='100%' src={ dish.image } alt={ dish.name} />
+				<CardImgOverlay>
+					<CardTitle>{ dish.name }</CardTitle>
+				</CardImgOverlay>
+			</Link>
+		</Card>
+	);
+}
 
-		this.state = {
-			selectedDish: null
-		}
-	}
+const Menu = (props) => {
 
-	onDishSelect(dish) {
-		this.setState({ selectedDish: dish });
-	}
-
-	render() {
-
-		const menu = this.props.dishes.map((dish) => {
-			return (
-				<div key={dish.id} className="col-12 col-md-5 m-1">
-					<Card onClick={() => this.onDishSelect(dish)}>
-						<CardImg width="100%" object src={dish.image} alt={dish.name} />
-						<CardImgOverlay>
-							<CardTitle>{dish.name}</CardTitle>
-						</CardImgOverlay>
-					</Card>
-				</div>
-			);
-		});
-
+	const menu = props.dishes.map((dish) => {
 		return (
-			<div className="container">
-				<div className="row">
-					{menu}
-				</div>
-				<DishDetail dish={this.state.selectedDish}/>
+			<div key={dish.id} className="col-12 col-md-5 m-1">
+				<Card>
+					<RenderMenuItem  dish={ dish }/>
+				</Card>
 			</div>
 		);
-	}
+	});
+
+	return (
+		<div className="container">
+			<div className="row">
+				<Breadcrumb>
+					<BreadcrumbItem>
+						<Link to='/home'>Home</Link>
+					</BreadcrumbItem>
+					<BreadcrumbItem active>
+						Menu
+					</BreadcrumbItem>
+				</Breadcrumb>
+				<div className="col-12">
+					<h3>Menu</h3>
+					<hr />
+				</div>
+			</div>
+			<div className="row">
+				{menu}
+			</div>
+		</div>
+	);
 }
 
 export default Menu;
